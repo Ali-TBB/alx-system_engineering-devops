514a4ec476a2259ac84fb36a42d2c9f701364f3d
Ali-TBB <Ali-Tbb@git.com>
2024-02-28 11:52:39 +0100

    0x06-regular_expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt*n/).join
+sender = ARGV[0][/from:([^\]]+)/, 1]
+receiver = ARGV[0][/to:([^\]]+)/, 1]
+flags = ARGV[0][/flags:([^\]]+)/, 1]
+
+# Output the extracted values
+puts "#{sender},#{receiver},#{flags}"
+  
