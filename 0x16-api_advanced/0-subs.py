41bd80430f54ea47941437bab2e0802829875dcf
Ali-TBB <Ali-Tbb@git.com>
2024-06-04 12:13:06 +0100

    some style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,13 +11,6 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Retrieve the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers for the subreddit.
-        Returns 0 if the subreddit is not valid.
     """
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
